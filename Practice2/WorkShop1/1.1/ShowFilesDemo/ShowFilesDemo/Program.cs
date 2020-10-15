773ba9262d9571b0bce2517a037f959be2157d5f
Vlad Bichenkov <vbichenko@bk.ru>
2020-10-15 13:35:28 +0300

    .

---
+++ Practice2/WorkShop1/1.1/ShowFilesDemo/ShowFilesDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ShowFilesDemo
+{
+    class Program
+    {
+        static void ShowDirectory(DirectoryInfo dir)
+        {
+            // Показать все файлы
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                Console.WriteLine("File: {0}", file.FullName);
+            }
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                ShowDirectory(subDir);
+            }
+        }
+            static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory); ShowDirectory(dir);
+        }
+    }
+}
---
+++ Practice2/WorkShop1/1.2/FileWatchingDemo/FileWatchingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileWatchingDemo
+{
+    class Program
+    {
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher =
+            new FileSystemWatcher(Environment.SystemDirectory);
+            watcher.Filter = "*.ini";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed +=
+            new FileSystemEventHandler(watcher_Changed);
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+    }
+}
