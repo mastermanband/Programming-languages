80a3ed43415d411d74d292ff02663716464e9170
Vlad Bichenkov <vbichenko@bk.ru>
2020-10-15 13:32:24 +0300

    .

---
+++ Pracrice1/WorkShop4/4.1/test/test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1;
+            Int32 i32 = 1;
+            double db = 1;
+
+            i16 = i32;
+            i16 = db;
+
+            i32 = i16;
+            i32 = db;
+
+            db = i16;
+            db = i32;
+
+
+        }
+    }
+}
