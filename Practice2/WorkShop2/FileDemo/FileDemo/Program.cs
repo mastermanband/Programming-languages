029980fd894b5d9e71fb5a26406265124019f778
Vlad Bichenkov <vbichenko@bk.ru>
2020-10-15 13:39:24 +0300

    .

---
+++ Practice2/WorkShop2/FileDemo/FileDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = File.CreateText(@"c:\newfile.txt"); writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?"); writer.Close();
+            StreamReader reader = File.OpenText(@"c:\newfile.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+        }
+    }
+}
