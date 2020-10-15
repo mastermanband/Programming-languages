f7da9e8280b4e985279741335f9d198ee0e65695
Vlad Bichenkov <vbichenko@bk.ru>
2020-10-15 13:21:30 +0300

    .

---
+++ Pracrice1/WorkShop2/2.2/SortString/SortString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SortString
+{
+    class Program
+    {
+        static string s = "Microsoft .NET Framework 2 Application Development Foundation";
+        static string[] sa = s.Split(" ");
+        static void Main(string[] args)
+        {
+            Array.Sort(sa);
+            s = string.Join(" ", sa);
+            Console.WriteLine(s);
+        }
+    }
+}
