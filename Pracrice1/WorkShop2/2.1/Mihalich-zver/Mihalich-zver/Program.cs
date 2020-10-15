bacbe6fa33709828726c65651a52988c965b9d2e
Vlad Bichenkov <vbichenko@bk.ru>
2020-10-15 13:16:10 +0300

    .

---
+++ Pracrice1/WorkShop2/2.1/Mihalich-zver/Mihalich-zver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Mihalich_zver
+{
+    class Program
+    {
+        static SByte a = 0;
+        static Byte b = 0;
+        static Int16 c = 0;
+        static Int32 d = 0;
+        static Int64 e = 0;
+        static string s = "";
+        static Exception ex = new Exception();
+        static object[] types = { a, b, c, d, e, s, ex };
+        static void Main(string[] args)
+        {
+            foreach (object o in types)
+            {
+                string type;
+                if (o.GetType().IsValueType) type = "Value type";
+                else
+                    type = "Reference Type";
+                Console.WriteLine("{0}: {1}", o.GetType(), type);
+            }
+        }
+    }
+}
