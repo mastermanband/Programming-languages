782dccb4cdd5646d6fa8eaf6a83f184212d98632
Vlad Bichenkov <vbichenko@bk.ru>
2020-11-04 13:12:35 +0300

    .

---
+++ Practice10/WorkShop2/avtomat_bandita/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace avtomat_bandita
+{
+    partial class Однорукий_бандит
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.go = new System.Windows.Forms.Button();
+            this.take = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dvg1 = new System.Windows.Forms.Timer(this.components);
+            this.dvg2 = new System.Windows.Forms.Timer(this.components);
+            this.dvg3 = new System.Windows.Forms.Timer(this.components);
+            this.stop1 = new System.Windows.Forms.Timer(this.components);
+            this.stop2 = new System.Windows.Forms.Timer(this.components);
+            this.stop3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 96F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 181);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 96F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(184, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 181);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 96F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(356, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 181);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(43, 270);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(135, 50);
+            this.go.TabIndex = 3;
+            this.go.Text = "Погнали!";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // take
+            // 
+            this.take.Location = new System.Drawing.Point(43, 329);
+            this.take.Name = "take";
+            this.take.Size = new System.Drawing.Size(135, 69);
+            this.take.TabIndex = 4;
+            this.take.Text = "Сделать ставку";
+            this.take.UseVisualStyleBackColor = true;
+            this.take.Click += new System.EventHandler(this.take_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(212, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Баланс: 100$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(212, 329);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Выбрать ставку: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(345, 329);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(212, 355);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(257, 29);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Осталось попыток: 0";
+            // 
+            // dvg1
+            // 
+            this.dvg1.Interval = 25;
+            this.dvg1.Tick += new System.EventHandler(this.dvg1_Tick);
+            // 
+            // dvg2
+            // 
+            this.dvg2.Interval = 25;
+            this.dvg2.Tick += new System.EventHandler(this.dvg2_Tick);
+            // 
+            // dvg3
+            // 
+            this.dvg3.Interval = 25;
+            this.dvg3.Tick += new System.EventHandler(this.dvg3_Tick);
+            // 
+            // stop1
+            // 
+            this.stop1.Interval = 1500;
+            this.stop1.Tick += new System.EventHandler(this.stop1_Tick);
+            // 
+            // stop2
+            // 
+            this.stop2.Interval = 2500;
+            this.stop2.Tick += new System.EventHandler(this.stop2_Tick);
+            // 
+            // stop3
+            // 
+            this.stop3.Interval = 3500;
+            this.stop3.Tick += new System.EventHandler(this.stop3_Tick);
+            // 
+            // Однорукий_бандит
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 453);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.take);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Однорукий_бандит";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.Button take;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer dvg1;
+        private System.Windows.Forms.Timer dvg2;
+        private System.Windows.Forms.Timer dvg3;
+        private System.Windows.Forms.Timer stop1;
+        private System.Windows.Forms.Timer stop2;
+        private System.Windows.Forms.Timer stop3;
+    }
+}
+
